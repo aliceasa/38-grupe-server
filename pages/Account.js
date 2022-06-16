acc876b4adb985b1e69f425d161f35bff8eb3893
Alicija <aknolia@rambler.ru>
2022-06-16 22:10:54 +0300

    push

--- pages/Account.js
+++ pages/Account.js
@@ -1,15 +1,15 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
-class PageLogin extends PageTemplate {
+class PageAccount extends PageTemplate {
   constructor(data) {
     super(data);
   }
 
   mainHTML() {
     return `<div class="row">
-                    <h1>Login page 🎅</h1>
+                    <h1>Account page 🎅</h1>
                 </div>`;
   }
 }
 
-export { PageLogin };
+export { PageAccount };
